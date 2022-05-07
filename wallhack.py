1b3526cd57e3e78487237e4a330758aa0345db6a
BinkBoinkBonk <38870983+BinkBoinkBonk@users.noreply.github.com>
2022-05-07 00:43:00 -0500

    Update wallhack.py
    Clean code

--- wallhack.py
+++ wallhack.py
@@ -22,29 +22,6 @@ def main():
     ''')
     pm = pymem.Pymem("csgo.exe")
     client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
-##    class ConVar():
-##        def __init__(self, name):
-##            try:
-##                self.address = 0
-##                interface_engine_cvar = 0x3E9EC
-##                vstdlib = process.module_from_name(pm.process_handle, 'vstdlib.dll').lpBaseOfDll
-##                v1 = pm.read_uint(vstdlib + interface_engine_cvar)
-##                v2 = pm.read_uint(pm.read_uint(pm.read_uint(v1 + 0x34)) + 0x4)
-##                while v2 != 0:
-##                    if name == pm.read_string(pm.read_uint(v2 + 0x0C)):
-##                        self.address = v2
-##                        return
-##                    # print(pm.read_string(pm.read_uint(a0 + 0x0C)))
-##                    v2 = pm.read_uint(v2 + 0x4)
-##            except Exception as err:
-##                pass
-##
-##        def set_int(self, value: int):
-##             pm.write_int(self.address + 0x30, value ^ self.address)
-##     
-##    grenadepreview = ConVar('cl_grenadepreview')
-##
-##    grenadepreview.set_int(1)
     while True:
         glow_manager = pm.read_int(client + dwGlowObjectManager)
 
